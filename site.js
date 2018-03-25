f39a289ac516e5460240562d79e2d38ebfc95d99
Sarina-Stoker <sstoker1@hawk.iit.edu>
2018-03-25 01:49:03 -0500

    Typo fixed. Forgot to add quotations around img.

--- site.js
+++ site.js
@@ -44,7 +44,7 @@ $("#slideshow > img:gt(0)").hide();
   setInterval(function() { 
   $('#slideshow > img:first')
     .fadeOut(1000)
-    .next(img).fadeIn(1000)
+    .next('img').fadeIn(1000)
     .end().appendTo('#slideshow');},  3000);
     
 })
