4db43f164e73d3a0a39a5cbdacd2a09f347eea1d
Sarina-Stoker <sstoker1@hawk.iit.edu>
2018-04-12 21:29:10 -0500

    Added prevent default function in order to show the console output.

--- site.js
+++ site.js
@@ -20,6 +20,7 @@ $('#payform').on('submit',
         /*Determing total cost of tickets*/
         var total= 12.95*adultT + 6.95*childT;
         console.log('The total is:'+total);
+        e.preventDefault();
       }
   
   )
