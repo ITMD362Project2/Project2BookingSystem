c3a1f99826712e63d0ef5acbf6d31347cea09f51
Sarina-Stoker <sstoker1@hawk.iit.edu>
2018-04-12 23:29:11 -0500

    Moving diplay code inside of costFunction.

--- site.js
+++ site.js
@@ -21,15 +21,15 @@ $('#payform').on('submit',
         var total= 12.95*adultT + 6.95*childT;
         console.log('The total is:'+total);
         
+        /*Displaying total on payment page*/
+      var t = document.getElementById(tot);
+      if(typeof t !== 'undefined' && t !== null) {
+      document.getElementById(tot).innerHTML = total.toString();
+      }
+        
       }
   
   )
-  
-  /*Displaying total on payment page*/
-    var t = document.getElementById(tot);
-    if(typeof t !== 'undefined' && t !== null) {
-    document.getElementById(tot).innerHTML = total.toString();
-      }
    
 
 
