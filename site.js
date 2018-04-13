7c157f0fd1a9f6403ff2b2ca21be0a821c8badea
Sarina-Stoker <sstoker1@hawk.iit.edu>
2018-04-13 01:54:45 -0500

    Going back to original code.

--- site.js
+++ site.js
@@ -14,12 +14,11 @@ $('#payform').on('submit',
  var total=0;
  $('#ticketSelection').on('submit',
     
-    /*getting values entered by user for number of tickets*/
+    function costFunction()
+      { 
+        /*getting values entered by user for number of tickets*/
         var adultT=document.getElementById("ad").value;
         var childT=document.getElementById("ch").value;
-        
-    function costFunction()
-      {
 
         /*Determing total cost of tickets*/
         total= 12.95*adultT + 6.95*childT;
@@ -33,7 +32,6 @@ $('#payform').on('submit',
     function displayCost()
   {
     /*Displaying total on payment page*/
-    total= 12.95*adultT + 6.95*childT;
     var t = document.getElementById("tot");
       if(typeof t !== 'undefined' && t !== null) {
         document.getElementById("tot").innerHTML = total.toString();
