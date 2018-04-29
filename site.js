7346658544356b3a3fc3c2066c9be57dd90ee617
Rosana Sincere <rosanasincere@gmail.com>
2018-04-29 09:15:33 -0500

    I changed the css. I can't get the js on the payment page to work tohugh.

--- site.js
+++ site.js
@@ -3,12 +3,12 @@
 
 
  /*Calculating cost of tickets*/
- 
+
  var total=0;
  $('#ticketSelection').on('submit',
-    
+
     function costFunction()
-      { 
+      {
         /*getting values entered by user for number of tickets*/
         var adultT=document.getElementById("ad").value;
         var childT=document.getElementById("ch").value;
@@ -17,23 +17,23 @@
         total= 12.95*adultT + 6.95*childT;
         /*This will add the value of the result to a hidden field I added to the form*/
         document.getElementById("result").value = total;
-       
-     }  
+
+     }
   )
-  
+
    function displayCost(){
-    
+
        var t = document.getElementById("tot");
        if(typeof t !== 'undefined' && t !== null) {
         /*var ans= document.getElementById("result").value;*/
         document.getElementById("tot").innerHTML = 100;
-       } 
+       }
     }
- 
+
 /*Empty Form Checker*/
 $('#payform').on('submit',
   function Empty(form)
-  { 
+  {
     if (document.getElementById("email").value == "") {
       alert( "Please enter your email address." );
       return false ;
@@ -58,7 +58,7 @@ $('#payform').on('submit',
       return true ;
     }
   })
-    
+
 
 /*Displays picture upon click of dots*/
 
